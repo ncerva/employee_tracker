2a10afe167d49f3f4ed2e61d7352bee896f09bc4
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-04-16 16:28:02 -0500

    tables created in sql

--- employeetracker_db.sql
+++ employeetracker_db.sql
@@ -1,1 +1,23 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+
 CREATE DATABASE employeetracker_db;
+USE employeetracker_db;
+
+CREATE TABLE employee(
+id INT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id  INT
+);
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
+);
+
