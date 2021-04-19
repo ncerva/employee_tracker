7e6b2f17122047174137aa45cb12258da694c56e
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-04-18 21:10:29 -0500

    mysql done

--- employeetracker_db.sql
+++ employeetracker_db.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
 CREATE TABLE employee(
-id INT PRIMARY KEY,
+id INT NOT_NULL PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
@@ -12,12 +12,36 @@ manager_id  INT
 );
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30),
 );
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
 );
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 200000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 120000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 100000, 2);
 
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Micheal", "Scott", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Stanley", "Hudson", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Creed","Bratton",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Dwight", "Schrute", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jim", "Halpert", 1, 6);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
--- server.js
+++ server.js
@@ -1,48 +1,69 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('dotenv').config();
+
+const db = require('db')
+db.connect({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+})
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'password',
+    password: 'pw',
     databse: 'employeetracker_db',
 });
 
-const promptUser = () => {
+const start = () => {
     inquirer.prompt([
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'choice',
-        choice: [
-            'View All Employees?',
-            'View All Employees By Roles?',
-            'View all Emplyees By Deparments',
-            'Update Employee',
-            'Add Employee?',
-            'Add Role?',
-            'Add Department?',]
-    }
-])
-.then(function(viewAl){
-    switch (viewAl.choice){
-        case 'View All Employees?':
-            viewAllEmployees();
-            break;
-        case 'View All Employees by Roles?':
-            viewAllRoles();
-            break;   
-        case 'View All Employees by Departments?':
-            viewAllDepartments ();
-            break;  
-        case 'Add Employee?':
-            addEmployee ();
-            break;  
-        case 'Add Role?':
-            addRole ();
-            break;  
-        case 'Add Department?':
-            addDepartment ();
-            break;
-    }
-});
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choice: [
+                'View All Employees?',
+                'View All Employees By Roles?',
+                'View all Emplyees By Deparments',
+                'Update Employee',
+                'Add Employee?',
+                'Add Role?',
+                'Add Department?',]
+        }
+    ])
+        .then(function (viewAl) {
+            switch (viewAl.choice) {
+                case 'View All Employees?':
+                    viewAllEmployees();
+                    break;
+                case 'View All Employees by Roles?':
+                    viewAllRoles();
+                    break;
+                case 'View All Employees by Departments?':
+                    viewAllDepartments();
+                    break;
+                case 'Add Employee?':
+                    addEmployee();
+                    break;
+                case 'Add Role?':
+                    addRole();
+                    break;
+                case 'Add Department?':
+                    addDepartment();
+                    break;
+            }
+        });
 };
+
+connection.connect((err) => {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+function init() {
+    promptUser()
+};
+init();
